c6637aa82884c0eda23892544092512bc952395a
SHUBHAM MISHRA <shubhammishra3121995>
2023-12-31 16:19:10 +0530

    Changes2

--- Package1/src/Program2.java
+++ Package1/src/Program2.java
@@ -5,6 +5,7 @@ public class Program2 {
         System.out.println("Hello, India-2");
         System.out.println("Hello, India-3");
         System.out.println("Hello, India-4");
-    }
+        System.out.println("Hello, India-5");
+        System.out.println("Hello, India-6");
 }
 
