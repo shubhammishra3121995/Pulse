766745fb25ab1830259979eb20602af47362e8e7
SHUBHAM MISHRA <shubhammishra3121995>
2023-12-31 14:52:19 +0530

    Added my project

---
+++ Package1/src/Program1.java
@@ -0,0 +1,10 @@
+public class Program1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World-1");
+        System.out.println("Hello, World-2");
+        System.out.println("Hello, World-3");
+        System.out.println("Hello, World-4");
+    }
+    }
+
---
+++ Package1/src/Program2.java
@@ -0,0 +1,10 @@
+public class Program2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, India-1");
+        System.out.println("Hello, India-2");
+        System.out.println("Hello, India-3");
+        System.out.println("Hello, India-4");
+    }
+}
+
